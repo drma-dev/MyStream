ecfe5f57d191c882af5f394b98c82ba6974aee2a
DF <donaldfa08@outlook.com>
2021-07-17 11:08:12 -0300

    Revert "removendo sessions"
    This reverts commit 1279ca9e33753b283415b6747fffa44810d501ad.

--- MyStream/Core/ApiCore.cs
+++ MyStream/Core/ApiCore.cs
@@ -25,16 +25,16 @@ namespace MyStream.Core
 
         public async static Task<T> GetSession<T>(this HttpClient http, ISyncSessionStorageService storage, string request_uri) where T : class
         {
-            //if (!storage.ContainKey(request_uri))
-            //{
+            if (!storage.ContainKey(request_uri))
+            {
                 var response = await http.GetAsync(request_uri);
 
                 if (!response.IsSuccessStatusCode) throw new NotificationException(response);
 
-            //storage.SetItem(request_uri, await response.Content.ReadFromJsonAsync<T>());
-            //}
-            return await response.Content.ReadFromJsonAsync<T>();
-            //return storage.GetItem<T>(request_uri);
+                storage.SetItem(request_uri, await response.Content.ReadFromJsonAsync<T>());
+            }
+
+            return storage.GetItem<T>(request_uri);
         }
 
         public async static Task<T> GetLocalRapid<T>(this HttpClient http, ISyncLocalStorageService storage, string request_uri) where T : class
