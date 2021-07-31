d04ff129613bb431c666afe72dfb44f23ca7a7a5
DF <donaldfa08@outlook.com>
2021-07-31 13:48:06 -0300

    popup catalog start

--- MyStream/Modal/Enum/TypeMedia.cs
+++ MyStream/Modal/Enum/TypeMedia.cs
@@ -1,8 +1,13 @@
-﻿namespace MyStream.Modal.Enum
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyStream.Modal.Enum
 {
     public enum TypeMedia
     {
+        [Display(Name = "Movies")]
         movie,
+
+        [Display(Name = "TV Shows")]
         tv
     }
 }
